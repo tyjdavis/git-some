16c49e9096d43334d8b1c96ce2a913449a011ce7
Tyler Davis <tyjdavis@outlook.com>
2017-04-25 22:16:35 -0400

    lvl 3 buttons not working yet

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   constructor () {
       super()
       this.focus = this.focus.bind(this);
+      this.class = this.class.bind(this);
       this.state = {
         user: {},
 
@@ -22,6 +23,14 @@ class App extends Component {
         this.setState({ user: response.data })
       })
     }
+
+    //could not get the below to work yet
+    class(){
+    axios.get('https://api.github.com/users/' + (this.value.value))
+    .then(response => {
+      this.setState({ user: response.data })
+    })
+  }
 
     componentDidMount() {
       axios.get('https://api.github.com/users/tyjdavis')
@@ -39,14 +48,16 @@ class App extends Component {
         <h1>GitHub info displayed using React.js!</h1>
         <User data={this.state.user}/>
         <section>
-          <p>The Iron Yard - Orlando</p>
-          <input className="waves-effect waves-light btn" type="button" value="Chrislebbano" ref={(value) => { this.textInput = value; }}  onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="Conshus" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="Ddsheard" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="Filusmarvin" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="JCR08" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="Ochanya" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
-          <input className="waves-effect waves-light btn" type="button" value="WJWJR" ref={(value) => { this.textInput = value; }} onClick={this.focus}/>
+          <p>The Iron Yard - Orlando Classmates</p>
+          
+          {/* these do not work yet */}
+          <input className="waves-effect waves-light btn" type="button" value="Chrislebbano" ref={(value) => { this.value = value; }}  onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="Conshus" ref={(value) => { this.value = value; }} onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="Ddsheard" ref={(value) => { this.value = value; }} onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="Filusmarvin" ref={(value) => { this.value = value; }} onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="JCR08" ref={(value) => { this.value = value; }} onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="Ochanya" ref={(value) => { this.value = value; }} onClick={this.class}/>
+          <input className="waves-effect waves-light btn" type="button" value="WJWJR" ref={(value) => { this.value = value; }} onClick={this.class}/>
         </section>
       </div>
     );
